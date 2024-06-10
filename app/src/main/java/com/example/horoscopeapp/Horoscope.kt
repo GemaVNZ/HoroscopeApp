e8a2fae1ad05f89d5306b1b0f411268b60027eed
GemaVNZ <gemavnz@gmail.com>
2024-06-10 11:34:14 +0200

    Empty proyecto.

---
+++ app/src/main/java/com/example/horoscopeapp/Horoscope.kt
@@ -0,0 +1,11 @@
+package com.example.horoscopeapp
+
+class Horoscope (
+    val id : String,
+    val name : String,
+    val logo : Int) {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/horoscopeapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.horoscopeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val horoscopeList : List<Horoscope> = listOf(
+        Horoscope(id="aries",name="Aries",logo=0),
+        Horoscope(id="taurus",name="Taurus",logo=1),
+        Horoscope(id="gemini",name="Gemini",logo=2),
+        Horoscope(id="cancer",name="Cancer",logo=3),
+        Horoscope(id="leo",name="Leo",logo=4),
+        Horoscope(id="virgo",name="Virgo",logo=5),
+        Horoscope(id="libra",name="Libra",logo=6),
+        Horoscope(id="scorpio",name="Scorpio",logo=7),
+        Horoscope(id="aries",name="",logo=8),
+        Horoscope(id="aries",name="",logo=9),
+        Horoscope(id="aries",name="",logo=10),
+        Horoscope(id="aquarius",name="Aquarius",logo=11),
+        Horoscope(id="pisces",name="Pisces",logo=12),
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
