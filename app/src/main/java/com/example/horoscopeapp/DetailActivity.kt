2b4242a092628bb783561ff06c7a6166e8b3b49c
GemaVNZ <gemavnz@gmail.com>
2024-06-12 13:55:06 +0200

    Añadimos otro activity.

--- app/src/main/java/com/example/horoscopeapp/DetailActivity.kt
+++ app/src/main/java/com/example/horoscopeapp/DetailActivity.kt
@@ -2,14 +2,26 @@ package com.example.horoscopeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val EXTRA_HOROSCOPE_ID = "HOROSCOPE_ID"
+    }
+
+    lateinit var horoscope:Horoscope
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val id = intent.getStringExtra("HOROSCOPE_ID")
-        findViewById<TextView>(R.id.textView).text = id
+        val id = intent.getStringExtra(EXTRA_HOROSCOPE_ID)
+        horoscope=HoroscopeProvider.findById(id!!)!!
+
+
+        findViewById<TextView>(R.id.textView).setText(horoscope.name)
+        findViewById<ImageView>(R.id.logoImageView).setImageResource(horoscope.logo)
     }
 }
--- app/src/main/java/com/example/horoscopeapp/HoroscopeAdapter.kt
+++ app/src/main/java/com/example/horoscopeapp/HoroscopeAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class HoroscopeAdapter (private val dataSet: List<Horoscope>, private val onItemClickListener: (Int) -> Unit)
